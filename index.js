b09fd8231469bec69b4da89e4f3b310f86022a2a
Mazbauddin <mazbauddin2010@gmail.com>
2024-05-14 04:40:51 +0600

    food added done

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ async function run() {
       res.send(result);
     });
 
+    // save a food data
+    app.post("/addFoodItem", async (req, res) => {
+      const newFoodItem = req.body;
+      const result = await allFoodsCollection.insertOne(newFoodItem);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
